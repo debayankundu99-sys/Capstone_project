daf61e3a2ff644e41f92a47c751c12e0184eb329
debayankundu99-sys <debayankundu99@gmail.com>
2026-06-08 17:28:59 +0530

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,19 @@
+variable "vpc_cidr" {
+    default = "10.0.0.0/16"
+  }
+
+  variable "public_subnets" {
+    default = ["10.0.1.0/24", "10.0.2.0/24"]
+  }
+
+  variable "private_subnets" {
+    default = ["10.0.3.0/24", "10.0.4.0/24"]
+  }
+
+  variable "key_name" {
+    default = "my-key"
+  }
+
+  variable "instance_type" {
+    default = "t2.micro"
+  }
